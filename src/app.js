9b7acda7f19c16f334f4fdb3dc30508fa119e420
Brent Carey <brentcarey@Brents-MacBook-Air.local>
2020-04-06 01:07:23 -0400

    Setup App for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('../utils/geocode')
 const forecast = require('../utils/forecast')
 
 const app = express();
+const port = process.env.PORT || 3000
 //Define paths for express config
 const publicPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -71,6 +72,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Listening on port 3000')
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
 })
